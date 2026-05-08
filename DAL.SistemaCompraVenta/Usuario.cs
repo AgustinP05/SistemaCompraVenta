371d5f8f87314304e66a57b58f3ea262ea14a2a7
Agustin <agustinperea5@gmail.com>
2026-05-08 17:45:15 -0300

    Se creo la clase Conexion ademas de clases varias en cap DAL

---
+++ DAL.SistemaCompraVenta/Usuario.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.SistemaCompraVenta
+{
+    public class Usuario
+    {
+        Conexion conexion = new Conexion();
+
+        public DataTable ObtenerUsuarios()
+        {
+            return conexion.LeerPorStoreProcedure("SP_ObtenerUsuarios");
+        }
+
+
+        public DataTable ListarUsuariosDataTable()
+        {
+            Conexion objConexion = new Conexion();
+
+            //La funcion devuelve un DataTable LeerPorComando
+            return objConexion.LeerPorComando(@"select usu.ID as ID_usuario, usu.Nombre as Nombre_Vendedor, ven.ID as ID_Venta, Valor_Total from tUsuario usu, tVenta ven Where usu.ID=ven.ID_Usuario;");
+        }
+    }
+}
--- UI.SistemaCompraVentas/LoginForm.cs
+++ UI.SistemaCompraVentas/LoginForm.cs
@@ -53,5 +53,6 @@ namespace UI.SistemaCompraVentas
                 MessageBox.Show("Credenciales incorrectas");
             }
         }
+
     }
 }
