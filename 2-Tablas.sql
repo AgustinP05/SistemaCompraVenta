6d9b1cbf5ccd54c045611608cc2ff2908ba5b25f
Agustin <agustinperea5@gmail.com>
2026-05-18 18:26:05 -0300

    Modificado Login para usar Usuarios de BD. Agregados scripts SQL

---
+++ 2-Tablas.sql
@@ -0,0 +1,15 @@
+﻿/* =========================================================
+   CREAR TABLA - Realizar una vez cada uno
+========================================================= */
+
+---- TABLA USUARIO ----
+CREATE TABLE tUsuario
+(
+	ID INT PRIMARY KEY IDENTITY(1,1),
+	Nombre VARCHAR(50) NOT NULL,
+	Password VARCHAR(50) NOT NULL,
+	Rol VARCHAR(50) NOT NULL
+);
+GO
+
+---- TABLA PRODUCTO ----
